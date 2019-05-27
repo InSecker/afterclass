83f3ece642453f19a31a10d1b874fdf550b40777
Pierre Secher <pierre@MacBook-Pro-de-Pierre.local>
2019-05-27 14:18:00 +0200

    adding structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'assets/config/bootstrap.php';
+$page_title = 'AfterClass';
+
+include  'assets/inc/header.php';
+
+?>
+
+<div class="">
+	<h1>Bienvenue sur AfterClass!</h1>
+</div>
+
+<?php
+
+include 'assets/inc/footer.php';
+
