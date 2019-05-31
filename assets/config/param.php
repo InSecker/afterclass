1fb2d0deec0b13783953352c8954f71f5b8e3bf6
Teddy <t.boirin@icloud.com>
2019-05-31 15:23:09 +0200

    modif bdd

--- assets/config/param.php
+++ assets/config/param.php
@@ -5,7 +5,7 @@
 
 // Paramètres pour PDO
 const DB_SGBD = 'mysql';
-const DB_HOST = 'db5000052448.hosting-data.io';
-const DB_DBNAME = 'dbs47385';
-const DB_USER = 'dbu135672';
-const DB_PASS = 'ax17zbtvh!';
+const DB_HOST = 'localhsot';
+const DB_DBNAME = 'afterclass';
+const DB_USER = 'root';
+const DB_PASS = 'root';
