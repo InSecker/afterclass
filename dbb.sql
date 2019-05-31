55a4f22fae2fe96f608b02da8092c3df6b0f1457
ChalalSamir <samir-94@outlook.com>
2019-05-31 14:28:48 +0200

    update bdd

--- dbb.sql
+++ dbb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 30, 2019 at 03:41 PM
+-- Generation Time: May 31, 2019 at 12:27 PM
 -- Server version: 5.7.25
 -- PHP Version: 7.3.1
 
@@ -21,25 +21,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `comments` (
-                          `id` int(11) NOT NULL,
-                          `post_id` int(11) NOT NULL,
-                          `author` varchar(20) NOT NULL,
-                          `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                          `content` text NOT NULL
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `author` varchar(20) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`id`, `post_id`, `author`, `date`, `content`) VALUES
-(1, 12, 'tareum', '2019-05-29 16:20:07', 'samir'),
-(2, 12, 'tareum', '2019-05-29 16:20:09', 'same'),
-(3, 13, 'Test6', '2019-05-30 10:04:06', 'Coucouuuuu'),
-(4, 13, 'Test6', '2019-05-30 10:16:10', 'hello'),
-(5, 18, 'Test4', '2019-05-30 13:52:10', 'C\'est naze'),
-(6, 25, 'Test4', '2019-05-30 15:12:59', 'bla'),
-(7, 24, 'Test4', '2019-05-30 15:22:22', 'Hello');
 
 -- --------------------------------------------------------
 
@@ -48,12 +35,12 @@ INSERT INTO `comments` (`id`, `post_id`, `author`, `date`, `content`) VALUES
 --
 
 CREATE TABLE `posts` (
-                       `id` int(11) NOT NULL,
-                       `title` varchar(50) NOT NULL,
-                       `content` longtext NOT NULL,
-                       `author` varchar(20) NOT NULL,
-                       `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                       `tag` int(3) DEFAULT NULL
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `content` longtext NOT NULL,
+  `author` varchar(20) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tag` int(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -61,18 +48,10 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `content`, `author`, `date`, `tag`) VALUES
-(6, 'Hello', 'Coucou', '', '2019-05-28 13:07:22', NULL),
-(9, 'Hello', 'Coucou', 'Test5', '2019-05-28 13:54:39', NULL),
-(12, 'J\'ai une question', 'Salut à tous je me demandais si ce que j\'étais en train de faire est vraiment utile ou non... Je suis déjà en retard sur le planning de la soirée', 'Test4', '2019-05-28 18:35:26', NULL),
-(15, 'Test', 'Test', 'Test6', '2019-05-30 10:40:14', NULL),
-(16, 'Test', 'Test', 'Test6', '2019-05-30 10:40:17', NULL),
-(17, 'Test', 'Test', 'Test6', '2019-05-30 10:40:19', NULL),
-(18, 'Test', 'Test', 'Test6', '2019-05-30 10:40:21', NULL),
-(19, '', '', 'Test4', '2019-05-30 13:52:32', NULL),
-(20, 'Test', 'test', 'Test4', '2019-05-30 13:53:31', NULL),
-(21, 'qdsfgh', 'qsdfgh', 'Test4', '2019-05-30 13:54:29', NULL),
-(22, 'Hello', 'GGGGG', 'Test4', '2019-05-30 14:10:41', NULL),
-(24, 'Test catégorie', 'TESTSTSTSTSTTSTSS', 'Test4', '2019-05-30 14:14:00', 3);
+(46, 'API YouTube API problem with insert to database', 'I wanted to make a connection to Youtube analytics which writes data to the database can be mysql or mariodb. Unfortunately, I have a problem with automatic authorization, you have a solution to this problem. It can be a script in php.\r\n\r\nSkorzystałem z kodu Harvey Connor: YouTube Analytics API php Samples', 'Pierre', '2019-05-31 12:00:15', 4),
+(47, 'Selecting an element not next to an element?', 'Currently i am trying to animate a div. I want to try and start the div using the :checked pseudo. I don\'t want the div I am transforming to be right next to the checkbox(for aesthetic reasons). So I currently have two div\'s that I am using. One for the checkbox and the other as a stage to play div\'s I want to animate/transform.\r\n\r\nI know you can select elements that are below another element as long as they are the child of the same element using, div + div{}.\r\n\r\nBut I am wondering if there is another way to select any element after a pseudo element goes into affect on another element(that is not in the same parent)?\r\n\r\nPure CSS or SCSS if their is a way. I know with Jquery I can easily do what I want but I want to stay away from JS for now, if there is another way.', 'Pierre', '2019-05-31 12:01:21', 2),
+(48, 'Is node js Node js code visible to client side', 'I heard that node js can be used in server side. I used jsp before. Inside jsp page java code is invisible to client. If node js is just javascript, then how it remains invisible to client?', 'Pierre', '2019-05-31 12:03:19', 5),
+(49, 'echo php', 'Salut\r\n\r\ncomment marche le echo en PHP ?\r\n\r\n<?php echo \"salut\";?>', 'teddylebg', '2019-05-31 12:17:00', 4);
 
 -- --------------------------------------------------------
 
@@ -81,8 +60,8 @@ INSERT INTO `posts` (`id`, `title`, `content`, `author`, `date`, `tag`) VALUES
 --
 
 CREATE TABLE `tags` (
-                      `id` int(11) NOT NULL,
-                      `label` varchar(50) NOT NULL
+  `id` int(11) NOT NULL,
+  `label` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -108,11 +87,11 @@ INSERT INTO `tags` (`id`, `label`) VALUES
 --
 
 CREATE TABLE `users` (
-                       `id` int(11) NOT NULL,
-                       `mail` varchar(100) NOT NULL,
-                       `username` varchar(20) NOT NULL,
-                       `type` varchar(20) NOT NULL,
-                       `password` varchar(72) NOT NULL
+  `id` int(11) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `password` varchar(72) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -120,13 +99,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `mail`, `username`, `type`, `password`) VALUES
-(19, 'pierre.secher@hetic.net', 'InSecker', 'user', 'TOTOTO'),
-(20, 'pierresecher98@hetic.net', 'Test1', 'user', '$2y$10$KQakIpaCiXejT.s6KKGDhOkn5/5cf6DML495TJ9oehAA4nCB8niba'),
-(21, 'trst@hetic.net', 'Test2', 'user', '$2y$10$mfbuMl5faFMqHsGxkSgSsO3n.h/VA9cGl7KqjxB7iGLd0G2KgcX8e'),
-(22, 'Test3@hetic.net', 'Test3', 'user', '$2y$10$Ej4ACxoe4jVIraP4Rxu90e..guYCXjory3hP/YE3OKR0qy2dTAxpm'),
-(23, 'Test4@hetic.net', 'Test4', 'user', '$2y$10$wiNoxUt7OKD9HZnCZRtJReOiNG0JrWE4fcTyEqnwokINO0c6sbPWC'),
-(24, 'test5@hetic.net', 'Test5', 'user', '$2y$10$8u3GLme1cEO5R47pAnyfnuxgV6PJCo6Lc8Pi8Z/GeSBHFUHCtTTs2'),
-(25, 'samir.chalal@hetic.net', 'tareum', 'user', '$2y$10$VwjdkUeQ9S304dsI2CQBW.AWl/0.i1NVo/mOQiLdl3dTrbHiwYfua');
+(26, 'pierre.secher@hetic.net', 'Pierre', 'user', '$2y$10$jhdbAXuIw999Xo0zjOrzree6etQql0rLm2BDdz9J7yoRFgyxNDPI6'),
+(27, 'samir94@hetic.net', 'Samir', 'user', '$2y$10$eNXfUn6TujdGsxY9XJ1afOkr8X/t2W/LmN57Us1YMq3Csd9FBN98y'),
+(29, 'teddyboirin@hetic.net', 'teddylebg', 'user', '$2y$10$xynBxyQdMYI7PspLM6WYlO3tw3CYaaWQw2SyfoIcBby3KrvVsFBVe');
 
 -- --------------------------------------------------------
 
@@ -135,11 +110,11 @@ INSERT INTO `users` (`id`, `mail`, `username`, `type`, `password`) VALUES
 --
 
 CREATE TABLE `votes` (
-                       `id` int(11) NOT NULL,
-                       `ref_id` int(11) NOT NULL,
-                       `ref` varchar(20) NOT NULL,
-                       `user` varchar(20) NOT NULL,
-                       `vote` varchar(4) NOT NULL
+  `id` int(11) NOT NULL,
+  `ref_id` int(11) NOT NULL,
+  `ref` varchar(20) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `vote` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -160,7 +135,17 @@ INSERT INTO `votes` (`id`, `ref_id`, `ref`, `user`, `vote`) VALUES
 (32, 6, 'post', 'Test6', 'up'),
 (33, 25, 'post', 'Test4', 'up'),
 (34, 18, 'post', 'Test4', 'up'),
-(35, 20, 'post', 'Test4', 'down');
+(35, 20, 'post', 'Test4', 'down'),
+(36, 26, 'post', 'tareum', 'down'),
+(37, 24, 'post', 'tareum', 'down'),
+(38, 9, 'post', 'tareum', 'up'),
+(39, 27, 'post', 'tareum', 'down'),
+(40, 22, 'post', 'tareum', 'up'),
+(41, 48, 'post', 'Pierre', 'up'),
+(42, 47, 'post', 'Pierre', 'up'),
+(43, 46, 'post', 'Pierre', 'up'),
+(44, 48, 'post', 'Samir', 'up'),
+(45, 47, 'post', 'teddylebg', 'up');
 
 --
 -- Indexes for dumped tables
@@ -204,13 +189,13 @@ ALTER TABLE `votes`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `tags`
@@ -222,10 +207,10 @@ ALTER TABLE `tags`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
